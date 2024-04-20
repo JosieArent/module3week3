1a400d194e261c7609ed7b7b8f0a8b35667715e0
Josie Arent <151769576+JosieArent@users.noreply.github.com>
2024-04-19 20:38:05 -0500

    Initial push to repo of our color console.

---
+++ index.js
@@ -0,0 +1,4 @@
+import { color, log, red, green } from 'console-log-colors';
+
+console.log (color.red("this is a NOT boring message."));
+console.log (color.green("this is a REALLY NOT a boring message."));
